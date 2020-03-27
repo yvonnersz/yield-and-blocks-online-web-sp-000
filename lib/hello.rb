956e2f8eb6ae92a82044199df1924a4099abf092
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 06:31:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,3 +9,18 @@ def hello_t(array)
 end
 
 # call your method here!
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+The block_given? me
